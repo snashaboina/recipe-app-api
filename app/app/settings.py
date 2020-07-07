e973cedae474810d301d0a29e2eba7cc2fcaac1b
snashaboina <suresh.iam.78gmail.comgit config --global user.name snashaboinagit config --global user.email suresh.iam.78gmail.com>
2020-07-07 03:01:31 +0100

    Added core app to the settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
