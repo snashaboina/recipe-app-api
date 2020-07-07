6552f972955976bc4d9c6a971c117b7cd9281c21
snashaboina <suresh.iam.78gmail.comgit config --global user.name snashaboinagit config --global user.email suresh.iam.78gmail.com>
2020-07-07 03:33:34 +0100

    commented unused imports in models.py and admin.py files

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,12 +1,18 @@
-# from django.test import TestCase
-# from django.contrib.auth import get_user_model
-#
-# class ModelTests(TestCase):
-#
-#     def test_create_user_with_email_successful(self):
-#         """
-#         Test creating a new user with an email is successful
-#         """
-#         email = "test@gmail.com"
-#         password = "Test123"
-#
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """
+        Test creating a new user with an email is successful
+        """
+        email = "test@gmail.com"
+        password = "Test123"
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.chech_password(password))
